eb0f96e25158bd8e8607cb9eae024e45db06342e
Biswojit Biswal <biswojitb474@gmail.com>
2024-03-22 16:25:29 +0530

    learning file upload using multer

--- Front-Backend/Backend/app.js
+++ Front-Backend/Backend/app.js
@@ -1,49 +1,53 @@
 // require('dotenv').config()
 // it is for node.js enviroment
 
-import 'dotenv/config'
+// import 'dotenv/config'
 import express from 'express';
 // it is for javascript environment and to solve this error we have to go to the package.json file and add "type": "module" after main.
 
 // const express = require('express');
 const app = express();
-const PORT =process.env.port || 3000;
+const PORT = 3000;
 
 
-// app.get("/", (req, res) => {
-//     res.send("hello world from server");
-// })
-
-app.get("/api/jokes", (req, res) => {
-    const jokes = [
-        {
-            id: 1,
-            title: 'A Joke',
-            content: 'This is my first joke'
-        },
-        {
-            id: 2,
-            title: 'Another Joke',
-            content: 'This is my Second joke'
-        },
-        {
-            id: 3,
-            title: 'Third Joke',
-            content: 'This is my Third joke'
-        },
-        {
-            id: 4,
-            title: 'Fourth Joke',
-            content: 'This is my Fourth joke'
-        },
-        {
-            id: 5,
-            title: 'Fifth Joke',
-            content: 'This is my Fifth joke'
-        },
-    ]
-    res.send(jokes);
+app.get("/", (req, res) => {
+    res.send(`heloo my server`);
 })
+app.get("/about", (req, res) => {
+    res.send(`<h1>hello world</h1>`);
+})
+
+
+// app.get("/api/jokes", (req, res) => {
+//     const jokes = [
+//         {
+//             id: 1,
+//             title: 'A Joke',
+//             content: 'This is my first joke'
+//         },
+//         {
+//             id: 2,
+//             title: 'Another Joke',
+//             content: 'This is my Second joke'
+//         },
+//         {
+//             id: 3,
+//             title: 'Third Joke',
+//             content: 'This is my Third joke'
+//         },
+//         {
+//             id: 4,
+//             title: 'Fourth Joke',
+//             content: 'This is my Fourth joke'
+//         },
+//         {
+//             id: 5,
+//             title: 'Fifth Joke',
+//             content: 'This is my Fifth joke'
+//         },
+//     ]
+//     res.send(jokes);
+// })
 
 
 
